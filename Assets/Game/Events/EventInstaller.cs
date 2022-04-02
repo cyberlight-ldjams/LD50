1ac22a3cadb59417f0c1849a8275b8d3d68b5537
Mike Knooihuisen <mknooihuisen@gmail.com>
2022-04-02 03:16:23 -0400

    working

---
+++ Assets/Game/Events/EventInstaller.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using Zenject;
+
+using System.Reflection;
+using System;
+using System.Linq;
+
+public class EventInstaller : MonoInstaller
+{
+    public override void InstallBindings()
+    {
+        //install signal bus into container
+        /**
+         * +SignalBusInstaller.Install(Container);
+        Debug.Log("Here");
+        //Grab all the IEvents
+        var events = Assembly.GetExecutingAssembly().GetTypes()
+            .Where(type => typeof(IEvent).IsAssignableFrom(type) && !type.IsInterface);
+
+
+        foreach (Type type in events)
+        {
+            Container.DeclareSignal(type);
+        }
+
+        Debug.Log("Done");
+        */
+    }
+}
---
+++ Assets/Game/Events/IEvent.cs
@@ -0,0 +1,4 @@
+public interface IEvent
+{
+    //we don't actually need anything but the existence of this interface... yet.
+}
