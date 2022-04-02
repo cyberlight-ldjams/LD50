1929f924b03b2c9bfa71ef7226676768d36e63fd
Mike Knooihuisen <mknooihuisen@gmail.com>
2022-04-02 03:58:39 -0400

    Zenject installed and IEvent system working.

--- Assets/Game/Events/EventInstaller.cs
+++ Assets/Game/Events/EventInstaller.cs
@@ -10,20 +10,16 @@ public class EventInstaller : MonoInstaller
     public override void InstallBindings()
     {
         //install signal bus into container
-        /**
-         * +SignalBusInstaller.Install(Container);
-        Debug.Log("Here");
+
+        SignalBusInstaller.Install(Container);
         //Grab all the IEvents
         var events = Assembly.GetExecutingAssembly().GetTypes()
             .Where(type => typeof(IEvent).IsAssignableFrom(type) && !type.IsInterface);
 
-
+        Debug.Log("here");
         foreach (Type type in events)
         {
             Container.DeclareSignal(type);
         }
-
-        Debug.Log("Done");
-        */
     }
 }
