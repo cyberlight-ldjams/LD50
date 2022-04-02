9ac24936ccf21a2009cd8a9b1f01f9e7eaada429
Jason M <johnred32@gmail.com>
2022-04-02 02:47:36 -0400

    Basic Character Movement Done
    Player can move and jump via physics

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -13,23 +13,62 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private float _speed = 10.0f;
 
+    [SerializeField]
+    private float _jumpForce = 10.0f;
+
+    [SerializeField]
+    private PlayerControls controls;
+
+    private bool grounded;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        controls = new PlayerControls();
+        controls.Player.Jump.performed += ctx => OnJump();
+        controls.Player.Move.performed += ctx => OnMove();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        _body.AddForce(_move.x * _speed, 0, _move.y * _speed);
+        if (grounded)
+        {
+            _body.AddForce(_move.x * _speed, 0, _move.y * _speed);
+        }
+        if (Mathf.Abs(_body.velocity.y) > 0.1)
+        {
+            grounded = false;
+        } 
+        else
+        {
+            grounded = true;
+        }
+        Debug.Log(_body.velocity);
     }
 
-    private void OnMove(InputValue playerMovement)
+    private void OnMove()
     {
-        Vector2 movement = playerMovement.Get<Vector2>();
+        Vector2 movement = controls.Player.Move.ReadValue<Vector2>();
         _move.x = movement.x;
         _move.y = movement.y;
-        Debug.Log(movement);
+    }
+
+    private void OnJump()
+    {
+        if (grounded)
+        {
+            _body.AddForce(0, _jumpForce, 0);
+        }
+    }
+
+    private void OnEnable()
+    {
+        controls.Player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Player.Disable();
     }
 }
