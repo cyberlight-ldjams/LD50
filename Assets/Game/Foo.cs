b4dd82453aa2cfab7a53e52f44c89c82ea1d1875
Mike Knooihuisen <mknooihuisen@gmail.com>
2022-04-02 20:22:11 -0400

    testing folder

--- Assets/Game/Foo.cs
+++ Assets/Game/Foo.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestScriptShouldExist : MonoBehaviour
+public class Foo : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
