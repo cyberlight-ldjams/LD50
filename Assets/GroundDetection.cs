695e8db53660f9f457fd62864222ed843fe148a8
Mike Knooihuisen <mknooihuisen@gmail.com>
2022-04-04 16:31:46 -0400

    ground detection

---
+++ Assets/GroundDetection.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class GroundDetection : MonoBehaviour
+{
+    [Inject]
+    private PlayerMovement pm;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        pm.IsGrounded = IsGrounded(other);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        pm.IsGrounded = false;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (!pm.IsGrounded)
+        {
+            pm.IsGrounded = IsGrounded(other);
+        }
+    }
+
+    private bool IsGrounded(Collider collider)
+    {
+        Vector3 point = collider.ClosestPointOnBounds(pm.transform.position);
+        return (collider.bounds.center.y < point.y);
+    }
+}
